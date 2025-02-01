8ef090b4ae83f429e2eb823586e50e690d741f15
RuidiH <ruidihuang@mymail.mines.edu>
2025-02-01 11:13:25 -0800

    adding hpp template for Renderer class

--- include/Renderer.hpp
+++ include/Renderer.hpp
@@ -11,17 +11,56 @@
 
 class Renderer
 {
-    public:
-        Renderer(const ObjectManager &objectManager, const Camera &camera);
-        ~Renderer();
+public:
+    Renderer(int width, int height, std::shared_ptr<Camera> camera)
+        : mScreenWidth(width), mScreenHeight(height), mCamera(camera);
+    ~Renderer();
 
-        void RenderSolidObjects();
-        void RenderTransparentObjects();
+    void Initialize();
 
-    private:
-        const ObjectManager &mObjectManager;
-        const Camera &mCamera;
+    void RenderScene(const std::vector<std::shared_ptr<GameObject>> &gameObjects,
+                     const std::shared_ptr<GameObject> &selectedObject,
+                     const glm::vec3 &lightPos);
 
+    GLuint GetShadowMapTexture() const;
+
+    const glm::mat4 &GetLightProjection() const;
+
+private:
+    // --- Render Passes ---
+    void RenderShadowPass(const std::vector<std::shared_ptr<GameObject>> &gameObjects,
+                          const glm::mat4 &lightProjection);
+    void RenderLightPass(const std::vector<std::shared_ptr<GameObject>> &gameObjects,
+                         const std::shared_ptr<GameObject> &selectedObject,
+                         const glm::vec3 &lightPos,
+                         const glm::mat4 &lightProjection);
+    void RenderHighlightPass(const std::vector<std::shared_ptr<GameObject>> &gameObjects,
+                             const std::shared_ptr<GameObject> &selectedObject);
+    void RenderCrosshair();
+
+private:
+    int mScreenWidth;
+    int mScreenHeight;
+    std::shared_ptr<Camera> mCamera;
+
+    // --- Shader Programs ---
+    Shader mMainShader;
+    Shader mShadowShader;
+    Shader mCrosshairShader;
+    Shader mHighlightShader;
+    // Shader mWaterShader;
+    // Shader mWaterShadowShader;
+
+    // --- Shadow Map Resources ---
+    GLuint mShadowMapFBO;
+    GLuint mShadowMapTexture;
+    glm::ivec2 mShadowResolution;
+    glm::mat4 mLightProjection;  // Typically orthographic * lookAt matrix
+
+    // --- Crosshair Resources ---
+    GLuint mCrosshairVAO;
+    GLuint mCrosshairVBO;
+    std::vector<float> mCrosshairVertices;
 };
 
 #endif
--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -15,33 +15,4 @@ Renderer::~Renderer()
 
 void Renderer::RenderSolidObjects()
 {
-    // // collect ptrs to each object type
-    // std::unordered_map<std::string, std::vector<std::shared_ptr<GameObject>>> objectMap;
-
-    // for (auto &object : mObjectManager.GetObjects())
-    // {
-    //     MeshComponent *meshComponent = object.second->GetComponent<MeshComponent>();
-
-    //     if (meshComponent->GetMeshType() == MeshType::CUBE)
-    //     {
-    //         objectMap[object.second->GetID()].push_back(object.second);
-    //     }
-    //     object.second->Render();
-    // }
-
-    // // find the texture information for each object type
-    // std::unordered_map<std::string, std::map<std::string, GLuint *>> textureMap;
-
-    // for (auto &obj : objectMap)
-    // {
-    //     if (textureMap.find(obj.first) != textureMap.end())
-    //     {
-    //         continue;
-    //     }
-
-    //     TextureComponent *textureComponent = obj.second[0]->GetComponent<TextureComponent>();
-    //     textureMap[obj.first] = textureComponent->GetTextureGroup();
-    // }
-
-    // 
 }
